fc2bfac93f676009b36a5926e3b41c0f42e5eb59
Administrator <Administrator@DESKTOP-ESDB78G>
2020-01-09 13:41:41 +0800

    作业

---
+++ Source/Chick/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Chick
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int q, w, u;
+            for (q = 0; q <= 20; q++)
+            {
+                for (w = 0; w <= 33; w++)
+                {
+                    u = 100 - q - w; if (3 * w + 5 * q + u / 3.0 == 100)
+                        Console.WriteLine("公鸡{0}只,母鸡{1}只,小鸡{2}只\n", q, w, u);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
